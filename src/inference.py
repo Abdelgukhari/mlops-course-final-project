43fa22b38e36926a2e1968d53273cae72bdbbd20
Abdelgukhari <abdelkab22@gmail.com>
2025-07-25 11:47:48 +0300

    Fix flake8 formatting issues

--- src/inference.py
+++ src/inference.py
@@ -1,27 +1,37 @@
-
 import pandas as pd
 import joblib
 from src.data_loader import load_data
 
+
 # Загрузка модели
 model = joblib.load("models/model.joblib")
+
 
 # Загрузка новых данных
 inference_data_path = "data/uber.csv"
 df = load_data(inference_data_path)
 
+
 # Выбор признаков
-features = ["pickup_longitude", "pickup_latitude", "dropoff_longitude", 
-            "dropoff_latitude", "passenger_count", "hour", "day"]
+features = [
+    "pickup_longitude", "pickup_latitude", "dropoff_longitude",
+    "dropoff_latitude", "passenger_count", "hour", "day"
+]
 X = df[features]
+
 
 # Прогнозирование
 predictions = model.predict(X)
 
+
 # Добавление предсказаний в DataFrame
 df["predicted_fare"] = predictions
 
+
 # Сохранение результатов
-df[["pickup_datetime", "predicted_fare"]].to_csv("inference_results.csv", index=False)
+df[["pickup_datetime", "predicted_fare"]].to_csv(
+    "inference_results.csv", index=False
+)
+
 
 print("Предсказания завершены. Результаты сохранены в 'inference_results.csv'")
